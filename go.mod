2f1f32c66b4cd9629b4bdb7fade4025d364f4105
Christopher Miles <akkadius1@gmail.com>
2021-10-17 14:40:46 -0500

    Add go web server, add deploy pipeline functionality

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/EQEmu/eqemu-docs-v2
+
+go 1.14
+
+require (
+	github.com/labstack/echo/v4 v4.6.1
+	github.com/otiai10/copy v1.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	copy "github.com/otiai10/copy"
+	"log"
+	"net/http"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	e := echo.New()
+	e.POST(
+		"/deploy", func(c echo.Context) error {
+			if c.Request().Header.Get("X-Github-Event") != "" &&
+				c.Request().Header.Get("X-Github-Delivery") != "" {
+
+				// run mkdocs build
+				start := time.Now()
+				cmd := exec.Command("mkdocs", "build")
+				_, err := cmd.Output()
+
+				if err != nil {
+					log.Println(err)
+				}
+
+				// copy from site to public
+				err = copy.Copy("site", "public")
+				if err != nil {
+					log.Println(err)
+				}
+
+				return c.JSON(
+					http.StatusOK,
+					echo.Map{
+						"data": "Deployed", "time": fmt.Sprintf("%s", time.Since(start)),
+					},
+				)
+			}
+
+			return c.JSON(http.StatusOK, echo.Map{"data": "Invalid request"})
+		},
+	)
+	e.Static("/", "public")
+	e.Logger.Fatal(e.Start(":8081"))
+}
