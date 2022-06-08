84004c4a9edebf8ef0f957ec17b77ca69f826b4d
Akkadius <akkadius1@gmail.com>
2022-06-08 00:29:18 -0500

    Push up adjustments

--- internal/console/util.go
+++ internal/console/util.go
@@ -1,5 +1,7 @@
 package console
 
+import "strings"
+
 func containsStringSlice(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
@@ -7,4 +9,16 @@ func containsStringSlice(s []string, e string) bool {
 		}
 	}
 	return false
+}
+
+func getStringInBetween(value string, a string, b string) string {
+	firstSplit := strings.Split(value, a)
+	if len(firstSplit) > 1 {
+		secondSplit := strings.Split(firstSplit[1], b)
+		if len(secondSplit) > 0 {
+			return strings.TrimSpace(secondSplit[0])
+		}
+	}
+
+	return ""
 }
